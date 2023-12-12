dca86f78dea0a714b0d1aa9a2f47b7102bedb453
ayhansimsek <ayhansimsek@hotmail.com>
2023-12-12 18:34:13 +1100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,27 +8,4 @@ document.getElementById('footer-placeholder').innerHTML = footer;
 
 
 
-
-
-//CCS calculator
-
-function calculateSubsidy() {
-    const incomeInput = document.querySelector('#income');
-    const income = Number(incomeInput.value);
-    let subsidyPercent;
-
-    if (income <= 80000) {
-        subsidyPercent = 90;
-    } else if (income > 80000 && income < 530000) {
-        subsidyPercent = 90 - Math.ceil((income - 80000) / 5000);
-        subsidyPercent = Math.max(subsidyPercent, 0); // it shoudnt go below 0%
-    } else {
-        subsidyPercent = 0;
-    }
-
-    const resultElement = document.querySelector('#result');
-    resultElement.textContent = `Your Childcare Subsidy is: ${subsidyPercent}%`;
-}
-
-
-document.querySelector('button').addEventListener('click', calculateSubsidy);  // Event listener for the button click
+console.log('header')
